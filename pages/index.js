f555c7fd7e75c8f19d8fe56ffc12a6c1e0ea46ef
MrWhite1338 <aleksa.marjanov@outlook.com>
2022-05-29 18:31:30 -0500

    implemented sanity as database, added hero banner

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,35 @@
 import React from "react";
 
-const Home = () => {
+import { client } from "./../lib/client";
+import { Product, FooterBanner, HeroBanner } from "../components/";
+
+// Passing in props from our getServerSideProps
+const Home = ({ products, bannerData }) => {
   return (
     <>
-      HeroBanner
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
+      {console.log(bannerData)}
+
       <div className="products-heading">
         <h2>Best Selling Products</h2>
-        <p>We bring you food from Balkan</p>
+        <p>We bring you best food from Balkan</p>
       </div>
       <div className="products-container">
-        {["Product 1", "Product 2"].map((product) => product)}
+        {products?.map((product) => product.name)}
       </div>
-      Footer
+      <FooterBanner />
     </>
   );
 };
 
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData },
+  };
+};
 export default Home;
